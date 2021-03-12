faf2b2cf42a634f611565ecc375c089879f8949b
uadson <uadsoncastro@gmail.com>
2021-03-12 18:51:56 -0300

    added querysets and published posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(
+        data_publicacao__lte=timezone.now()).order_by('-data_publicacao')
+    return render(request, 'blog/post_list.html', {'posts': posts})
